ae1dc8743dad8c0e689457cc41b0fbfed7978520
Jordan <jordandavis16@gmail.com>
2022-10-26 14:35:02 -0400

    hi

--- install_conda.sh
+++ install_conda.sh
@@ -5,7 +5,7 @@ mkdir -p ~/miniconda
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda/miniconda.sh
 bash ~/miniconda/miniconda.sh -b -u -p ~/miniconda
 rm -rf ~/miniconda/miniconda.sh
-source miniconda/bin/activate
+source ~miniconda/bin/activate
 sudo chown -R $USER:$USER ~/miniconda
 conda update -n base -c defaults conda -y
 conda config --set auto_activate_base true
